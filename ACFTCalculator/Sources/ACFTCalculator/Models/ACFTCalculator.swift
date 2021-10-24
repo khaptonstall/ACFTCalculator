1a4c64c7d6993e6e36f526e50773de4b2cfadb58
Kyle Haptonstall <khaptonstall93@gmail.com>
2021-10-24 18:44:16 -0500

    Added the ability to calculate points for the deadlift event.

--- ACFTCalculator/Sources/ACFTCalculator/Models/ACFTCalculator.swift
+++ ACFTCalculator/Sources/ACFTCalculator/Models/ACFTCalculator.swift
@@ -2,10 +2,14 @@ import Foundation
 
 public final class ACFTCalculator {
 
+    public enum ACFTEvent {
+        case threeRepetitionMaximumDeadlift(pounds: Int)
+    }
+
     // MARK: Properties
 
     /// Represents the column of possible points one can receive for an ACFT event.
-    let points: [String]
+    let points: [Int]
 
     /// Represents the column of pounds used for the 3-repetition maximum deadlift event.
     let deadliftPounds: [String]
@@ -32,7 +36,14 @@ public final class ACFTCalculator {
 
     public init() throws {
         let csvReader = try CSVReader.acftScoringStandardsReader()
-        self.points = try csvReader.readColumn(.points)
+
+        self.points = try csvReader.readColumn(.points).map {
+            guard let int = Int($0) else {
+                throw ACFTCalculatorError.csvReadingFailure(reason: .pointsNotIntRepresentable(value: $0))
+            }
+            return int
+        }
+
         self.deadliftPounds = try csvReader.readColumn(.deadlift)
         self.standingPowerThrowMeters = try csvReader.readColumn(.standingPowerThrow)
         self.handReleasePushUpRepetitions = try csvReader.readColumn(.handReleasePushUp)
@@ -41,5 +52,37 @@ public final class ACFTCalculator {
         self.plankTimes = try csvReader.readColumn(.plank)
         self.twoMileRunTimes = try csvReader.readColumn(.twoMileRun)
     }
-}
 
+    // MARK: Calculating Points
+
+    /// Calculates points earned for a given event.
+    /// - Parameter event: The `ACFTEvent` and data necessary to calculate points earned.
+    public func calculatePoints(for event: ACFTEvent) -> Int {
+        switch event {
+        case .threeRepetitionMaximumDeadlift(let pounds):
+            return self.calculatePointsForDeadlift(pounds: pounds)
+        }
+    }
+
+    private func calculatePointsForDeadlift(pounds: Int) -> Int {
+        for (index, value) in self.deadliftPounds.enumerated() {
+            // Skip over any empty strings.
+            guard !value.isEmpty else {
+                continue
+            }
+
+            // Because not all possible pounds values are listed in the CSV, if the
+            // given input pounds value is greater than or equal to the current pounds
+            // value, the input pounds value will fall under the current pounds point value.
+            // Force-unwrapping as tests exist to validate CSV data is convertable to Int.
+            guard pounds >= Int(value)! else {
+                continue
+            }
+
+            return self.points[index]
+        }
+
+        // For any value less than lowest possible pounds value, return 0 points.
+        return 0
+    }
+}
--- ACFTCalculator/Tests/ACFTCalculatorTests/ACFTCalculatorTests.swift
+++ ACFTCalculator/Tests/ACFTCalculatorTests/ACFTCalculatorTests.swift
@@ -24,7 +24,55 @@ final class ACFTCalculatorTests: XCTestCase {
         XCTAssertEqual(calculator.legTuckRepetitions.count, expectedLength)
         XCTAssertEqual(calculator.plankTimes.count, expectedLength)
         XCTAssertEqual(calculator.twoMileRunTimes.count, expectedLength)
+    }
 
+    func testDeadliftValuesAreConvertableToInt() throws {
+        let calculator = try ACFTCalculator()
+
+        for value in calculator.deadliftPounds {
+            // Skip over any empty strings.
+            guard !value.isEmpty else {
+                continue
+            }
+            // Verify any non-empty strings are convertable to Int.
+            XCTAssertNotNil(Int(value))
+        }
+    }
+
+    // MARK: Deadlift Calculator Tests
+
+    func testCalculatingPointsForListedPoundsValueReturnsCorrectPointsValue() throws {
+        let calculator = try ACFTCalculator()
+
+        // Use a pounds value that is explicitly listed on the CSV.
+        // 140 pounds should equate to 60 points.
+        let pounds = 140
+        let points = calculator.calculatePoints(for: .threeRepetitionMaximumDeadlift(pounds: pounds))
+        XCTAssertEqual(points, 60)
+    }
+
+    func testCalculatingPointsForUnlistedPoundsValueReturnsPointsForNextSmallestPoundsValue() throws {
+        let calculator = try ACFTCalculator()
+
+        // Use a pounds value that is not explicitly listed on the CSV.
+        // 140 pounds equates to 60 points, and the next value is 130 which
+        // equates to 50 points. A value of 139 should also equate to 50 points.
+        let pounds = 139
+        let points = calculator.calculatePoints(for: .threeRepetitionMaximumDeadlift(pounds: pounds))
+        XCTAssertEqual(points, 50)
+    }
+
+    func testCalculatingPointsForPoundsValueLessThanMinimumValueReturnsZeroPoints() throws {
+        let calculator = try ACFTCalculator()
+
+        // Use a pounds value that is less than the minimum pounds value (80 pounds).
+        let pounds = 0
+        let lastPoundsValue = try XCTUnwrap(calculator.deadliftPounds.last)
+        let minimumPoundsValue = try XCTUnwrap(Int(lastPoundsValue))
+        XCTAssertLessThan(pounds, minimumPoundsValue)
+
+        // Verify a points value of 0 is returned.
+        XCTAssertEqual(calculator.calculatePoints(for: .threeRepetitionMaximumDeadlift(pounds: pounds)), 0)
     }
 
 }
