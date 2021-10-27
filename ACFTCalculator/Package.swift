b7d9879f82642b6c2a7c22756566158307a897ed
Kyle Haptonstall <khaptonstall93@gmail.com>
2021-10-26 20:31:31 -0500

    Add watchOS

--- ACFTCalculator/Package.swift
+++ ACFTCalculator/Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     platforms: [
         .iOS(.v14),
         .tvOS(.v14),
-        .macOS(.v11)
+        .macOS(.v11),
+        .watchOS(.v6)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
